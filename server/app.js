db01f932a7b0e455803a186872d6a668c5b02b09
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-07-05 17:41:23 -0400

    Installing dependacies and packages

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false }));
