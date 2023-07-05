6dc3d264b39d064470ddddbb18f5af2b1b706458
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-07-05 17:56:34 -0400

    App is listening

--- server/app.js
+++ server/app.js
@@ -4,9 +4,29 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-dotenv = require('dotenv');
+// dotenv = require('dotenv');
 dotenv.config();
+//To access configuration file when needed 
 
 app.use(cors());
+// Used to send data on the back end when I have an API call
 app.use(express.json());
+// In order to send data in json format 
 app.use(express.urlencoded({extended : false }));
+
+// create
+// app.post('/insert', (request, response) => {
+
+// });
+
+// read
+app.get('/getAll', (request, response) => {
+  console.log('/test');
+
+});
+// update
+
+// delete
+
+
+app.listen(process.env.PORT, () => console.log('app is running'))
